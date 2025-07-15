a3b0e2923ddd1ec37c6d82fe65b09580c96d267f
Neeraj-kurmi <146584412+Neeraj-kurmi@users.noreply.github.com>
2025-07-15 19:19:18 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -35,12 +35,12 @@ const Projects = () => {
       </div>
       <div className="py-12 px-8 flex flex-wrap gap-5">
         <div className="p-3 md:p-6 flex flex-col w-80 shadow-xl shadow-slate-900 rounded-2xl bg-[#0c0e19]">
-          <img src='\vlog.png' className="md:h-40 h-40 m-4 "/>
+          <img src='\trust.png' className="md:h-40 h-40 m-4 "/>
           <h3 className=" px-3 text-xl md:text-2xl font-bold leading-normal">
-            Vlog Website
+            Real Trust Website
           </h3>
           <p className="px-4 text-sm md:text-md leading-tight py-2">
-            This is a fontend project user can join groups and explore social environment moments and created in React.js and tailwind.
+             This is a FullStack website.Created by using MERN Stack technology
           </p>
           <div className="mt-2 p-2 flex gap-2 md:gap-4">
             <button
