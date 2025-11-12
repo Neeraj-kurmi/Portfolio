7c0b015c86b3a2693fda585331ddd0dce0a424e1
Neeraj-kurmi <146584412+Neeraj-kurmi@users.noreply.github.com>
2025-11-12 19:34:05 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,14 +13,14 @@ const About = () => {
                 <ImArrowRight2 size={30} className="mt-1"/>
                 <span className='w-96'>
                     <h1 className='text-xl md:text-2xl font-semibold leading-normal'>Welcome to My Portfolio!</h1>
-                    <p>Hi there! I’m Neeraj Kurmi, a passionate web developer specializing in the MERN stack (MongoDB, Express.js, React, Node.js). With a deep love for creating seamless, dynamic web applications, I thrive on turning complex problems into elegant solutions.</p>
+                    <p>Hi there! I’m Neeraj Kurmi, a passionate web developer specializing in the Full stack (MongoDB, Spring boot , SQL , React, Node.js). With a deep love for creating seamless, dynamic web applications, I thrive on turning complex problems into elegant solutions.</p>
                 </span>
             </div>
             <div className='flex gap-3 py-4'>
                 <ImArrowRight2 size={30} className="mt-1"/>
                 <span className='w-96'>
                     <h1 className='text-xl md:text-2xl font-semibold leading-normal'>My Journey</h1>
-                    <p>I began my journey in tech with a curiosity for how websites are built and the technology behind them. This fascination led me to dive deep into web development, where I discovered the power of the MERN stack. With its robust features and flexibility, I found it to be the perfect toolkit for crafting modern web applications that are both responsive and user-friendly.</p>
+                    <p>I began my journey in tech with a curiosity for how websites are built and the technology behind them. This fascination led me to dive deep into web development, where I discovered the power of the Full stack. With its robust features and flexibility, I found it to be the perfect toolkit for crafting modern web applications that are both responsive and user-friendly.</p>
                 </span>
             </div>
             <div className='flex gap-3 py-4'>
@@ -34,7 +34,7 @@ const About = () => {
                 <ImArrowRight2 size={30} className="mt-1"/>
                 <span className='w-96'>
                     <h1 className='text-xl md:text-2xl font-semibold leading-normal'> Backend Developer</h1>
-                    <p>I'm backend developer and love to work with express,node.js mongodb.</p>
+                    <p>I'm backend developer and love to work with spring boot , mongodb , SQL.</p>
                 </span>
             </div>
           </ul>
