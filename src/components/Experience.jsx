5b6a918cb02a445cb7a035e0b3b5c85223f34dc4
Neeraj-kurmi <146584412+Neeraj-kurmi@users.noreply.github.com>
2025-11-13 13:19:23 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -9,7 +9,7 @@ const Experience = () => {
   return (
     <div id="Experience" className='p-10 md:p-24'>
         <h3 className='text-2xl md:text-4xl text-white font-bold'>Experience</h3>
-        <h1 className=" text-white px-3 text-xl md:text-2xl font-bold leading-normal pt-2"> > Technologies I Used</h1>
+        <h1 className=" text-white px-3 text-xl md:text-2xl font-bold leading-normal pt-2">  Technologies I Used</h1>
            <div className='flex flex-wrap md:w-2/5 gap-8 md:p-12 py-20'>
             <span className='p-3 bg-zinc-950 flex items-center rounded-2xl'>
                 <FaHtml5 color="#E34F26" size={50}/>
